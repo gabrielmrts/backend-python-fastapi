5b13ba93d57f7bd6377dea75750c36247a456918
Gabriel <gabrielmrtss@gmail.com>
2024-11-06 16:19:51 -0300

    refactor: simplify the application entrypoint

--- app/main.py
+++ app/main.py
@@ -3,7 +3,6 @@ from fastapi.middleware.cors import CORSMiddleware
 from contextlib import asynccontextmanager
 from app.api.routes import router
 from typing import AsyncGenerator
-import os
 from app.core.init_db import init_database
 from app.core.settings import Settings
 
@@ -18,16 +17,14 @@ async def lifespan(app: FastAPI) -> AsyncGenerator[None, None]:
     init_database()
     yield
 
-is_dev = (os.environ.get("ENV") not in ["live", "test"])
-
 settings = Settings()  # type: ignore
 app = FastAPI(
     title=settings.PROJECT_TITLE,
     description=settings.PROJECT_DESCRIPTION,
     version=settings.PROJECT_VERSION,
     lifespan=lifespan,
-    docs_url="/docs" if is_dev else None,
-    redoc_url="/redoc" if is_dev else None
+    docs_url="/docs",
+    redoc_url="/redoc"
 )
 
 app.add_middleware(
